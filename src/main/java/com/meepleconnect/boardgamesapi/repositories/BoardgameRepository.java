2db507ea0b1a73ff5b270d013e8411fee5a32da0
Ruthger de Graaf <Johannes.vanburen@kpnmail.nl>
2025-02-16 13:23:33 +0100

    Updated bg repo

--- src/main/java/com/meepleconnect/boardgamesapi/repositories/BoardgameRepository.java
+++ src/main/java/com/meepleconnect/boardgamesapi/repositories/BoardgameRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BoardgameRepository extends JpaRepository<Boardgame, Long> {
-    List<Boardgame> findByGenre(String genre);
-    List<Boardgame> findByAvailableTrue();
+
+    List<Boardgame> findByGenreIgnoreCase(String genre);
+
+    Optional<Boardgame> findByNameIgnoreCase(String name);
+}
