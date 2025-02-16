26fa958670cca4d9f8431cc0e03e19860867f01e
Ruthger de Graaf <Johannes.vanburen@kpnmail.nl>
2025-02-16 13:26:42 +0100

    Fixed meepleconnectboardgamesapiapplication.java

---
+++ src/test/java/com/meepleconnect/boardgamesapi/MeepleConnectBoardgamesApiApplication.java
@@ -0,0 +1,12 @@
+package com.meepleconnect.boardgamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeepleConnectBoardgamesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
