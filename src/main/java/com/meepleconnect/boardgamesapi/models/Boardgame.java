eda683aeeaad3f8bf66e88ae69122d3effe83452
Ruthger de Graaf <Johannes.vanburen@kpnmail.nl>
2025-02-16 16:37:21 +0100

    Change boardgame model

--- src/main/java/com/meepleconnect/boardgamesapi/models/Boardgame.java
+++ src/main/java/com/meepleconnect/boardgamesapi/models/Boardgame.java
@@ -20,67 +20,37 @@ public class Boardgame {
     @JoinColumn(name = "publisher_id")
     private Publisher publisher;
 
-    public String getName() {
-        return name;
-    }
+    public Boardgame() {}
 
-    public double getPrice() {
-        return price;
-    }
-
-    public int getExpansions() {
-        return expansions;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public int getMinPlayers() {
-        return minPlayers;
-    }
-
-    public int getMaxPlayers() {
-        return maxPlayers;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public Publisher getPublisher() {
-        return publisher;
-    }
-
-    public void setName(String name) {
+    public Boardgame(String name, double price, int expansions, boolean available, 
+                     int minPlayers, int maxPlayers, String genre, Publisher publisher) {
         this.name = name;
-    }
-
-    public void setPrice(double price) {
         this.price = price;
-    }
-
-    public void setExpansions(int expansions) {
         this.expansions = expansions;
-    }
-
-    public void setAvailable(boolean available) {
         this.available = available;
-    }
-
-    public void setMinPlayers(int minPlayers) {
         this.minPlayers = minPlayers;
-    }
-
-    public void setMaxPlayers(int maxPlayers) {
         this.maxPlayers = maxPlayers;
-    }
-
-    public void setGenre(String genre) {
         this.genre = genre;
-    }
-
-    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
+
+    public Long getId() { return id; }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getExpansions() { return expansions; }
+    public boolean isAvailable() { return available; }
+    public int getMinPlayers() { return minPlayers; }
+    public int getMaxPlayers() { return maxPlayers; }
+    public String getGenre() { return genre; }
+    public Publisher getPublisher() { return publisher; }
+
+    public void setName(String name) { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+    public void setExpansions(int expansions) { this.expansions = expansions; }
+    public void setAvailable(boolean available) { this.available = available; }
+    public void setMinPlayers(int minPlayers) { this.minPlayers = minPlayers; }
+    public void setMaxPlayers(int maxPlayers) { this.maxPlayers = maxPlayers; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public void setPublisher(Publisher publisher) { this.publisher = publisher; }
 }
