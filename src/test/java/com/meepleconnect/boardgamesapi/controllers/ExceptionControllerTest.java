54ee2938a9ad2d1902464f66497ece8dbc66ef14
Ruthger de Graaf <ruthger.degraaf@print.com>
2025-03-09 11:09:58 +0100

    Reactivated rest again.

--- src/test/java/com/meepleconnect/boardgamesapi/controllers/ExceptionControllerTest.java
+++ src/test/java/com/meepleconnect/boardgamesapi/controllers/ExceptionControllerTest.java
@@ -1,43 +1,43 @@
-//package com.meepleconnect.boardgamesapi.controllers;
-//
-//import com.meepleconnect.boardgamesapi.exceptions.GameNotFoundException;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class ExceptionControllerTest {
-//
-//    private ExceptionController exceptionController;
-//
-//    @BeforeEach
-//    void setUp() {
-//        exceptionController = new ExceptionController();
-//    }
-//
-//    @SuppressWarnings("null")
-//    @Test
-//    void handleGameNotFoundException_ShouldReturn404() {
-//        GameNotFoundException exception = new GameNotFoundException("Bordspel niet gevonden");
-//
-//        ResponseEntity<Object> response = exceptionController.handleGameNotFoundException(exception);
-//
-//        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-//        assertNotNull(response.getBody(), "Response body should not be null");
-//        assertTrue(response.getBody().toString().contains("Bordspel niet gevonden"));
-//    }
-//
-//    @SuppressWarnings("null")
-//    @Test
-//    void handleGlobalException_ShouldReturn500() {
-//        Exception exception = new Exception("Onverwachte fout");
-//
-//        ResponseEntity<Object> response = exceptionController.handleGlobalException(exception);
-//
-//        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-//        assertNotNull(response.getBody(), "Response body should not be null");
-//        assertTrue(response.getBody().toString().contains("Onverwachte fout"));
-//    }
-//}
+package com.meepleconnect.boardgamesapi.controllers;
+
+import com.meepleconnect.boardgamesapi.exceptions.GameNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExceptionControllerTest {
+
+    private ExceptionController exceptionController;
+
+    @BeforeEach
+    void setUp() {
+        exceptionController = new ExceptionController();
+    }
+
+    @SuppressWarnings("null")
+    @Test
+    void handleGameNotFoundException_ShouldReturn404() {
+        GameNotFoundException exception = new GameNotFoundException("Bordspel niet gevonden");
+
+        ResponseEntity<Object> response = exceptionController.handleGameNotFoundException(exception);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNotNull(response.getBody(), "Response body should not be null");
+        assertTrue(response.getBody().toString().contains("Bordspel niet gevonden"));
+    }
+
+    @SuppressWarnings("null")
+    @Test
+    void handleGlobalException_ShouldReturn500() {
+        Exception exception = new Exception("Onverwachte fout");
+
+        ResponseEntity<Object> response = exceptionController.handleGlobalException(exception);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertNotNull(response.getBody(), "Response body should not be null");
+        assertTrue(response.getBody().toString().contains("Onverwachte fout"));
+    }
+}
